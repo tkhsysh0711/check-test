653ac301c66691107cafe0ba4f8b4c12bc68e840
yoshi <ahg945127@gmail.com>
2024-01-16 12:01:04 +0900

    できた所まで

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -8,6 +8,19 @@
     <link rel="stylesheet" href="{{ asset('css/confirm.css') }}" />
 </head>
 <body>
-    
+    <header class="header">
+        <div class="header__inner">
+            <h1 class="header__logo">FashionablyLate</h1>
+        </div>
+    </header>
+
+    <main>
+        <h2 class="confirm__title">Confirm</h2>
+
+        <div class="form__button">
+            <button class="form__button--submit">送信</button>
+            <button class="form__button--back">修正</button>
+        </div>
+    </main>
 </body>
 </html>
